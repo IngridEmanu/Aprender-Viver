2d6b5cde612e922b5d4c88b9503d7cb16a236410
IngridEmanu <73448288+IngridEmanu@users.noreply.github.com>
2021-10-04 17:55:46 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <title>Praticando básico HTML/CSS</title>
+    <link rel="stylesheet"href="estilo.css" type="text/CSS">
+  </head>
+  <body class="tela-azul">
+    Aqui vai o codigo HTML que fara seu site aparecer.
+  </body>
+</html>
